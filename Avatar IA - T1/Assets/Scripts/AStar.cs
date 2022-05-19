972865b4705652586aef4088164913c65ac0ff21
TheoCaldas <theocaldas28@gmail.com>
2022-05-19 19:01:28 -0300

    [ADD] Import BinaryHeap implementation

--- Avatar IA - T1/Assets/Scripts/AStar.cs
+++ Avatar IA - T1/Assets/Scripts/AStar.cs
@@ -1,12 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using BinaryHeap;
 
 public class AStar
 {
-    void aStar(Tile startTile, Tile endTile)
+    public void aStar(Tile startTile, Tile endTile)
     {
+        int capacity = 80 * 200;
+        int maxDistance = 2000000;
+        BinaryHeap<int, int> queue = new BinaryHeap<int, int>(capacity, -1, maxDistance);
+        queue.Enqueue(3, 0);
+        queue.Enqueue(4, 2);
+        queue.Enqueue(5, 1);
+        queue.Enqueue(6, 8);
+        queue.Enqueue(7, 8);
 
+        while (queue.Count() > 0)
+            Debug.Log(queue.Dequeue());
     }
-
 }
--- Avatar IA - T1/Assets/Scripts/MapManager.cs
+++ Avatar IA - T1/Assets/Scripts/MapManager.cs
@@ -4,22 +4,17 @@ using UnityEngine;
 
 public class MapManager: SingletonMonoBehaviour<MapManager>
 {
-
     public Tile[,] tileMap;
     public List<Tile> eventTiles;
 
     public (int, int) currentPosition = new (0, 0);
 
     private void Start() {
-
+        AStar algo = new AStar();
+        algo.aStar(new Tile('A', 0, 0), new Tile('A', 0, 0));
     }
 
     private void Update() {
         
     }
-
-
-
-
-
 }
