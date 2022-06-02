08585441cc0420848627330808be34cb361dec13
TheoCaldas <theocaldas28@gmail.com>
2022-06-02 10:40:56 -0300

    [FIX] Appa position

--- Avatar IA - T1/Assets/Scripts/PathFollower.cs
+++ Avatar IA - T1/Assets/Scripts/PathFollower.cs
@@ -32,6 +32,9 @@ public class PathFollower
             Vector3 targetPosition = new Vector3( objective.position.x, transform.position.y, objective.position.z) ;
             transform.LookAt( targetPosition ) ;
             transform.localEulerAngles += new Vector3(0, 90, 0);
+            Transform t = transform.Find("Appa");
+            if (t != null)
+                t.localEulerAngles = new Vector3(0, 0, 0);
         }
     }
 
