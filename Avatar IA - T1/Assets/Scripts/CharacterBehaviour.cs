e8679c59d4110c93b4d57a9c751a09d86b159f82
TheoCaldas <theocaldas28@gmail.com>
2022-06-02 06:00:37 -0300

    [ADD] Line Mov

--- Avatar IA - T1/Assets/Scripts/CharacterBehaviour.cs
+++ Avatar IA - T1/Assets/Scripts/CharacterBehaviour.cs
@@ -9,20 +9,46 @@ public class CharacterBehaviour : MonoBehaviour
     public float period;
     public float amplitude;
     private float currentTime;
+    private bool isSlow;
+
+    public float lineInterval = 1.0f;
+    public float xOffset;
+    public float zOffset;
+    private Vector3 linePos;
     void Start()
     {
         originalPos = transform.localPosition;
         currentTime = 0.0f;
+        isSlow = false;
+        linePos = new Vector3(xOffset, originalPos.y, zOffset);
     }
 
     // Update is called once per frame
     void Update()
     {
         currentTime += Time.deltaTime;
-        float offset = amplitude * Mathf.Sin(currentTime * period);
-        if (offset < 0.0f)
-            offset = 0.0f;
+        int timeCost = 1;
+        if (MapManager.Instance.follower.hasPath())
+            timeCost = MapManager.Instance.follower.getCurrentTimeCost();
+        float yOffset = amplitude * Mathf.Sin(currentTime * period * 3.0f);
+        
+        if (timeCost >= 15)
+        {
+            isSlow = true;
+            yOffset = 0.0f;
+            currentTime = 0.0f;
+        }
+        else if (isSlow)
+        {
+            isSlow = false;
+            currentTime = 0.0f;
+        }
+            
+        if (yOffset < 0.0f)
+            yOffset = 0.0f;
 
-        transform.localPosition = new Vector3(originalPos.x, originalPos.y + offset, originalPos.z);
+        transform.localPosition = Vector3.Lerp(originalPos, linePos, currentTime / lineInterval);
+        transform.localPosition += new Vector3(0, yOffset, 0);
+        // MapManager.Instance.objectLookAtEvent(transform);
     }
 }
--- Avatar IA - T1/Assets/Scripts/MapManager.cs
+++ Avatar IA - T1/Assets/Scripts/MapManager.cs
@@ -127,15 +127,15 @@ public class MapManager: SingletonMonoBehaviour<MapManager>
         return n;
     }
 
-    private void characterLookAtEvent()
+    public void objectLookAtEvent(Transform obj)
     {
         if (currentEventIndex < eventTiles.Count - 1)
         {
             Transform objective = eventTiles[currentEventIndex + 1].tile3DRef.transform;
-            if (Vector3.Distance(objective.position, character.transform.position) > 1.0)
+            if (Vector3.Distance(objective.position, obj.position) > 1.0)
             {
-                Vector3 targetPosition = new Vector3( objective.position.x, character.transform.position.y, objective.position.z) ;
-                character.transform.LookAt( targetPosition ) ;
+                Vector3 targetPosition = new Vector3( objective.position.x, obj.position.y, objective.position.z) ;
+                obj.LookAt( targetPosition ) ;
             }
         }
     }
@@ -147,7 +147,7 @@ public class MapManager: SingletonMonoBehaviour<MapManager>
 
     private void Update() 
     {
-        characterLookAtEvent();
+        objectLookAtEvent(character.transform);
 
         if (currentState == MapState.Ready && currentEventIndex < eventTiles.Count - 1)
         {
